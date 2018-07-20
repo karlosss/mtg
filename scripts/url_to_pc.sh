e787e8317fe7f1cc250599e233dc14bc9bd29581
Karel Jilek <jilekka2@fit.cvut.cz>
2018-07-20 22:26:48 +0200

    added mtgtop8

--- scripts/url_to_pc.sh
+++ scripts/url_to_pc.sh
@@ -7,6 +7,12 @@ then
     num=$(echo "$url" | cut -d"/" -f5)
     wget "https://www.mtggoldfish.com/deck/download/$num" -O /tmp/deck.txt &>/dev/null
     /home/karlos/mtg/scripts/human_to_pc.sh <(cat /tmp/deck.txt) /dev/stdout
+elif grep 'mtgtop8\.com' <<< "$url" &>/dev/null
+then
+    wget "$url" -O /tmp/deck.txt &>/dev/null
+    url=$(cat /tmp/deck.txt | grep 'Export <a href=' | sed -E 's|.*Export <a href=(.*)>MTGO</a>.*|\1|g')
+    wget "http://www.mtgtop8.com/$url" -O /tmp/deck.txt &>/dev/null
+    /home/karlos/mtg/scripts/human_to_pc.sh <(cat /tmp/deck.txt | tr -d '\r' | grep -v '^Sideboard$') /dev/stdout
 else
     echo "Unsupported site!"
 fi
