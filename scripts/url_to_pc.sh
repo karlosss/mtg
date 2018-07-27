9f673cc94474eb269f1c3156762d7a72ece57a4c
Karel Jilek <jilekka2@fit.cvut.cz>
2018-07-27 13:42:35 +0200

    fixed mtggoldfish archetypes

--- scripts/url_to_pc.sh
+++ scripts/url_to_pc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 url=$(echo "$1" | cut -d"#" -f1)
+wget "$url" -O /tmp/deck.txt &>/dev/null
 
 if grep 'mtggoldfish\.com' <<< "$url" &>/dev/null
 then
-    num=$(echo "$url" | cut -d"/" -f5)
-    wget "https://www.mtggoldfish.com/deck/download/$num" -O /tmp/deck.txt &>/dev/null
+    url=$(cat /tmp/deck.txt | grep 'Download</a>' | sed -E 's|.*href="(.*)">Download</a>|\1|g')
+    wget "https://www.mtggoldfish.com/$url" -O /tmp/deck.txt &>/dev/null
     /home/karlos/mtg/scripts/human_to_pc.sh <(cat /tmp/deck.txt) /dev/stdout
 elif grep 'mtgtop8\.com' <<< "$url" &>/dev/null
 then
-    wget "$url" -O /tmp/deck.txt &>/dev/null
     url=$(cat /tmp/deck.txt | grep 'Export <a href=' | sed -E 's|.*Export <a href=(.*)>MTGO</a>.*|\1|g')
     wget "http://www.mtgtop8.com/$url" -O /tmp/deck.txt &>/dev/null
     /home/karlos/mtg/scripts/human_to_pc.sh <(cat /tmp/deck.txt | tr -d '\r' | grep -v '^Sideboard$') /dev/stdout
